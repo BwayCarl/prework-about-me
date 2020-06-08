159fdd1656de666edf1bb7fbc28b2d4830cb808a
Carlos Martinez <66034611+BwayCarl@users.noreply.github.com>
2020-06-08 16:05:00 -0400

    Update Prework folder with Module assignments.

---
+++ PREWORK_CM/Module-6/prework-js-basics/javascript.js
@@ -0,0 +1,78 @@
+/* Button 1 - Grow */
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "400px";
+});
+
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.width = "400px";
+});
+
+/* Button 2 - Blue */
+document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.backgroundColor = "blue";
+});
+
+/* Button 3 - Fade */
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.opacity = "0.5";
+});
+
+/* Button 4 - Reset */
+document.getElementById("button4").addEventListener("click", function () {
+
+    document.getElementById("box").style.height = "150px";
+ });
+
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.width = "150px";
+});
+
+ document.getElementById("button4").addEventListener("click", function () {
+
+    document.getElementById("box").style.backgroundColor = "orange";
+});
+
+document.getElementById("button4").addEventListener("click", function () {
+
+     document.getElementById("box").style.opacity= "1";
+
+});
+
+document.getElementById("button4").addEventListener("click", function () {
+
+        document.getElementById("box").style.borderRadius = "0%";    
+});
+
+/* Button 5 - Red */
+document.getElementById("button5").addEventListener("click", function(){
+
+    document.getElementById("box").style.backgroundColor ="red";
+});
+
+/* Button 6 - Green */
+document.getElementById("button6").addEventListener("click", function(){
+
+    document.getElementById("box").style.backgroundColor ="green";
+});
+
+/* Button 7 - Circle */
+document.getElementById("button7").addEventListener("click", function(){
+
+    document.getElementById("box").style.borderRadius ="50%";
+});
+
+document.getElementById("button7").addEventListener("click", function(){
+
+    document.getElementById("box").style.backgroundColor = "purple";
+});
+
+/* Button 8 - Green */
+document.getElementById("button8").addEventListener("click", function(){
+
+    document.getElementById("box").style.backgroundColor ="white";
+});
